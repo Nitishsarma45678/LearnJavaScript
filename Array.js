5eac43d45b9eb642a72a67d0b2112a63ed8c74b4
Nitishsarma45678 <nitishsarma8@gmail.com>
2025-02-28 01:07:54 +0530

    Add test.js with a simple log statement

---
+++ Array.js
@@ -0,0 +1,11 @@
+let num = [1,2,3,4,5];
+
+function sumArray(arr){
+    let sum = 0;
+    for(let i = 0 ; i<arr.length ; i ++){
+        sum += arr[i]
+    }
+    return sum;
+}
+
+console.log(sumArray(num))
