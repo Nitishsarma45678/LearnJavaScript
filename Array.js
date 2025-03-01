957d1f942c6e6094defd826f56e7b5ae090946c1
Nitishsarma45678 <nitishsarma8@gmail.com>
2025-03-02 00:59:27 +0530

    New practice questions added with solving approaches

--- Array.js
+++ Array.js
@@ -12,3 +12,16 @@
 
 
 
+// 1. Print all elements of an array
+
+// Input: [1, 2, 3, 4, 5]
+// Output: 1 2 3 4 5
+
+
+let arr = [1, 2, 3 , 4, 5];
+ 
+for(i = 0 ; i <= arr.length ; i++){
+    console.log(arr[i]);                    //approach 1 
+}
+
+arr.forEach(element => console.log(element));   //approach 2 (.forEach method)
