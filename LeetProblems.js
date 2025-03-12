f313296ee94ba08eebdb0bce04493733dc79cbd6
Nitishsarma45678 <nitishsarma8@gmail.com>
2025-03-12 19:58:35 +0530

    Solved LeetCode Problems

--- LeetProblems.js
+++ LeetProblems.js
@@ -30,3 +30,33 @@ let g = "anagram"
 let t = "nagaram"
 
 console.log(isSame(s,t));
+
+
+
+// <<----------------------------------------------------------------------------------------------------->>
+
+
+// 2529. Maximum Count of Positive Integer and Negative Integer
+// Difficulty: Easy
+function findMaxCount(nums){
+    let pos = 0;
+    let neg = 0;
+
+    for (let num of nums){
+        if (num > 0){
+            pos++ ;
+        }
+        else if(num < 0){
+            neg++ ;
+        }
+    }
+    return Math.max(pos , neg);
+}
+
+
+console.log(maxCount([-2, -1, -1, 1, 2, 3]));
+
+console.log(maxCount([-3, -2, -1, 0, 0, 1, 2])); 
+
+console.log(maxCount([5, 20, 66, 1314])); 
+
