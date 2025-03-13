96d97aa498d95e132d26ac5ea08ca9e15a3e5995
Nitishsarma45678 <nitishsarma8@gmail.com>
2025-03-13 20:16:09 +0530

    New Solved LeetCode Prob

--- LeetProblems.js
+++ LeetProblems.js
@@ -1,91 +1,91 @@
 
-// // 217. Contains Duplicate
-// // Difficulty: Easy
+// 217. Contains Duplicate
+// Difficulty: Easy
 
-// let testArray = [1,2,3,1]
+let testArray = [1,2,3,1]
 
-// let s = new Set(testArray)
+let s = new Set(testArray)
 
-// if( s.size != testArray.length){
-//     console.log(true);
-// }
-// else{
-//     console.log(false);
-// }
+if( s.size != testArray.length){
+    console.log(true);
+}
+else{
+    console.log(false);
+}
 
-// // <<----------------------------------------------------------------------------------------------------->>
+// <<----------------------------------------------------------------------------------------------------->>
 
-// // 242. Valid Anagram
-// // Difficulty: Easy
+// 242. Valid Anagram
+// Difficulty: Easy
 
-// function isSame(g,t){
-//     if (g.length != t.length){
-//         return false;
-//     }
-//     return g.split('').sort().join('') === t.split('').sort().join('');
+function isSame(g,t){
+    if (g.length != t.length){
+        return false;
+    }
+    return g.split('').sort().join('') === t.split('').sort().join('');
 
-// }
+}
 
-// let g = "anagram"
-// let t = "nagaram"
+let g = "anagram"
+let t = "nagaram"
 
-// console.log(isSame(s,t));
-
-
-
-// // <<----------------------------------------------------------------------------------------------------->>
+console.log(isSame(s,t));
 
 
 
-// // 2529. Maximum Count of Positive Integer and Negative Integer
-// // Difficulty: Easy
-
-// function findMaxCount(nums){
-//     let pos = 0;
-//     let neg = 0;
-
-//     for (let num of nums){
-//         if (num > 0){
-//             pos++ ;
-//         }
-//         else if(num < 0){
-//             neg++ ;
-//         }
-//     }
-//     return Math.max(pos , neg);
-// }
-
-
-// console.log(maxCount([-2, -1, -1, 1, 2, 3]));
-
-// console.log(maxCount([-3, -2, -1, 0, 0, 1, 2])); 
-
-// console.log(maxCount([5, 20, 66, 1314])); 
+// <<----------------------------------------------------------------------------------------------------->>
 
 
 
-// // <<----------------------------------------------------------------------------------------------------->>
+// 2529. Maximum Count of Positive Integer and Negative Integer
+// Difficulty: Easy
+
+function findMaxCount(nums){
+    let pos = 0;
+    let neg = 0;
+
+    for (let num of nums){
+        if (num > 0){
+            pos++ ;
+        }
+        else if(num < 0){
+            neg++ ;
+        }
+    }
+    return Math.max(pos , neg);
+}
+
+
+console.log(maxCount([-2, -1, -1, 1, 2, 3]));
+
+console.log(maxCount([-3, -2, -1, 0, 0, 1, 2])); 
+
+console.log(maxCount([5, 20, 66, 1314])); 
 
 
 
-// // 9. Palindrome Number
-// // Difficulty: Easy
+// <<----------------------------------------------------------------------------------------------------->>
 
-// function checkPalin(x){
-//     if (x < 0){
-//         return false;
-//     }
 
-//     const str = x.toString();
 
-//     const reversedStr = str.split('').reverse().join('')
+// 9. Palindrome Number
+// Difficulty: Easy
 
-//     return str === reversedStr;
-// }
+function checkPalin(x){
+    if (x < 0){
+        return false;
+    }
 
-// console.log(checkPalin(121))
-// console.log(checkPalin(-121))
-// console.log(checkPalin(10))
+    const str = x.toString();
+
+    const reversedStr = str.split('').reverse().join('')
+
+    return str === reversedStr;
+}
+
+console.log(checkPalin(121))
+console.log(checkPalin(-121))
+console.log(checkPalin(10))
 
 
 
