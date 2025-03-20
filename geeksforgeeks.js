ae1ac4537adb7e7801423648483ef703081e9470
Nitishsarma45678 <nitishsarma8@gmail.com>
2025-03-20 12:09:43 +0530

    geeksforgeeks array questions solved

--- geeksforgeeks.js
+++ geeksforgeeks.js
@@ -18,3 +18,24 @@ function getAlternates(arr){
 
 let arr = [10 , 20 , 30 , 40 , 50 , 60]
 console.log(getAlternates(arr).join(' '));
+
+
+
+
+// Input: arr[] = [1, 2, 3, 4], x = 3
+// Output: 2
+// Explanation: There is one test case with array as [1, 2, 3 4] and element to be searched as 3. Since 3 is present at index 2, the output is 2.
+
+function linearFind(arr , target){
+    for(i = 0; i < arr.length ; i++){
+        if (arr[i] == target){
+            return i;
+        }
+    }
+    return -1;
+}
+ 
+let inArr = [1, 2, 3, 4]
+let target = 3 ;    // x value
+
+console.log(linearFind(inArr , target));
