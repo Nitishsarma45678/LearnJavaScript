b8a10b021b75dabb96d4992924be80fe54e0d270
Nitishsarma45678 <nitishsarma8@gmail.com>
2025-03-25 20:10:48 +0530

    Logical Problems

---
+++ SolveJSlogic.js
@@ -0,0 +1,31 @@
+// Given the existing code below, write some code to switch the values of the variables a and b.
+
+
+// function test() {
+//     var a = "3";
+//     var b = "8";
+    
+// /***********Do not change the code above 👆*******/
+
+
+// /***********Do not change the code below 👇*******/
+
+//     console.log("a is " + a);
+//     console.log("b is " + b);
+// }
+
+
+// Do NOT change any of the existing code.
+
+// You are NOT allowed to type any numbers.
+
+// You should NOT redeclare the variables a and b.
+
+// Use exactly 3 lines of code to solve the problem.
+
+// Output:
+// When the code is run, it should output:
+
+// a is 8  
+// b is 3  
+
